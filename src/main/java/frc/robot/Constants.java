0b05546b5d76feb40cdb33c63a994f9a7ed08a99
Team 9567 Programing <Team9567Prog@gmail.com>
2024-01-27 14:55:25 -0800

    Added all of the Robot Chasis constents

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,20 +1,29 @@
 package frc.robot;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
-    // Port numbers for driver and operator gamepads. These correspond with the numbers on the USB
+    // Port numbers for driver and operator gamepads. These correspond with the
+    // numbers on the USB
     // tab of the DriverStation
-    
+
     public static final int kDriverControllerPort = 0;
-    //public static final int kOperatorControllerPort = 1;
+    // public static final int kOperatorControllerPort = 1;
   }
 
   public static class DrivetrainConstants {
@@ -37,7 +46,8 @@ public final class Constants {
     public static final int kLauncherCurrentLimit = 80;
     public static final int kFeedCurrentLimit = 80;
 
-    // Speeds for wheels when intaking and launching. Intake speeds are negative to run the wheels
+    // Speeds for wheels when intaking and launching. Intake speeds are negative to
+    // run the wheels
     // in reverse
     public static final double kLauncherSpeed = 1;
     public static final double kLaunchFeederSpeed = 1;
@@ -45,5 +55,18 @@ public final class Constants {
     public static final double kIntakeFeederSpeed = -.2;
 
     public static final double kLauncherDelay = 1;
+  }
+
+  public static class autonomousCommand {
+
+  }
+
+  public static class RobotChassisConstants {
+    public static final int kCurrentLimit = 240 / 4;
+    public static final int kLeftCanId = 1;
+    public static final int kRightCanId = 3;
+    public static final int kLeftFollowerCanId = 2;
+    public static final int kRightFollowerCanId = 4;
+    public static final IdleMode kMotorBrakeMode = IdleMode.kBrake;
   }
 }
--- src/main/java/frc/robot/subsystems/RobotChassis.java
+++ src/main/java/frc/robot/subsystems/RobotChassis.java
@@ -4,31 +4,32 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
-
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 //import com.revrobotics.CANSparkMax.IdleMode;
+import frc.robot.Constants.RobotChassisConstants;
 
 public class RobotChassis extends SubsystemBase {
-    public CANSparkMax leftCanSparkMax = new CANSparkMax(1, MotorType.kBrushless);
-    public CANSparkMax rightCanSparkMax = new CANSparkMax(3, MotorType.kBrushless);
-    public CANSparkMax leftFollowerCanSparkMax = new CANSparkMax(2, MotorType.kBrushless);
-    public CANSparkMax rightFollowerCanSparkMax = new CANSparkMax(4, MotorType.kBrushless);
+    public CANSparkMax leftCanSparkMax = new CANSparkMax(RobotChassisConstants.kLeftCanId, MotorType.kBrushless);
+    public CANSparkMax rightCanSparkMax = new CANSparkMax(RobotChassisConstants.kRightCanId, MotorType.kBrushless);
+    public CANSparkMax leftFollowerCanSparkMax = new CANSparkMax(RobotChassisConstants.kLeftFollowerCanId, MotorType.kBrushless);
+    public CANSparkMax rightFollowerCanSparkMax = new CANSparkMax(RobotChassisConstants.kRightFollowerCanId, MotorType.kBrushless);
     public DifferentialDrive drivetrain = new DifferentialDrive(leftCanSparkMax, rightCanSparkMax);
     public double targetSpeed = 0;
     public double targetTurn = 0;
     public double currentSpeed = 0;
     public double currentTurn = 0;
 
-    public RobotChassis(){
+    public RobotChassis() {
 
-        for (CANSparkMax m : new CANSparkMax[] { leftCanSparkMax, rightCanSparkMax, leftFollowerCanSparkMax,rightFollowerCanSparkMax}) {
+        for (CANSparkMax m : new CANSparkMax[] { leftCanSparkMax, rightCanSparkMax, leftFollowerCanSparkMax,
+                rightFollowerCanSparkMax }) {
             m.clearFaults();
-            m.setIdleMode(IdleMode.kCoast);
-            m.setSmartCurrentLimit(240 / 4, 240 / 4);
+            m.setIdleMode(RobotChassisConstants.kMotorBrakeMode);
+            m.setSmartCurrentLimit(RobotChassisConstants.kCurrentLimit, RobotChassisConstants.kCurrentLimit);
         }
-        
+
         leftCanSparkMax.setInverted(false);
         rightCanSparkMax.setInverted(true);
         leftFollowerCanSparkMax.setInverted(true);
@@ -43,29 +44,30 @@ public class RobotChassis extends SubsystemBase {
         targetTurn = turn * -1;
         updateSpeed();
     }
-    public void periodic(){
+
+    public void periodic() {
         updateSpeed();
     }
-    public void updateSpeed(){
+
+    public void updateSpeed() {
         var speedDiffrence = targetSpeed - currentSpeed;
         var turnDiffrence = targetTurn - currentTurn;
-        if (speedDiffrence > .5){
+        if (speedDiffrence > .5) {
             currentSpeed += .15;
-        }else if (speedDiffrence > .15){
+        } else if (speedDiffrence > .15) {
             currentSpeed += .1;
-        }else{
+        } else {
             currentSpeed = targetSpeed;
         }
 
-        if (turnDiffrence > 0.5){
+        if (turnDiffrence > 0.5) {
             currentTurn += .15;
-        }else if (speedDiffrence > .15){
+        } else if (speedDiffrence > .15) {
             currentTurn += .1;
-        }else{
+        } else {
             currentTurn = targetTurn;
         }
         drivetrain.arcadeDrive(currentSpeed, currentTurn);
     }
 
 }
-
