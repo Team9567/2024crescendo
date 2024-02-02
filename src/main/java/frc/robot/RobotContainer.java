ed490fea2486d4f1a9464a5169226e0a9db2db78
Team 9567 Programing <Team9567Prog@gmail.com>
2024-02-01 20:18:00 -0800

    Working soft low gear code. Ramped speed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,10 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.commands.PrepareLaunch;
+import frc.robot.commands.LaunchNote;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.RobotChassis;
 import frc.robot.subsystems.RobotLauncher;
 import frc.robot.subsystems.Vision;
@@ -76,9 +80,10 @@ public class RobotContainer {
     // holding the
     // left Bumper
     driver.button(2).whileTrue(launcher.getIntakeCommand());
-  }
-
+    //Set up for the binding for the soft low gear
+    driver.button(3).onTrue(new InstantCommand(()->{chassis.setLowGear();}));
+    driver.button(3).onFalse(new InstantCommand(()->{chassis.setHighGear();}));
   // public Command getAutonomousCommand() {
   // return new DriveDistanceCommand(chassis);
-  // }
+  }
 }
