9ad40cb5c986e2133dc3adb67ccc5d9c82664dd8
Team 9567 Programming <148642470+Team9567Prog@users.noreply.github.com>
2024-02-14 19:57:20 -0800

    readded drive team feedback comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -92,3 +92,31 @@ public class RobotContainer {
     //}
   }
 }
+//Controller
+//Shoot B - current
+
+//Source intake, X - current
+
+//Floor intake, press button hold down, pick up ring, when picked up, retract, button A, let her have the option to retrack mannually store position
+//Intake button, move down, hold to move motors on the intake motors to move
+
+
+//One button for ready position A stays within frame perameter ready for amp out of frame perimeter then back to Amp
+//Store low, no note button A
+//Store high, stores high Button A
+
+
+//place in AMP, button Y, intake takeover with button push. From the High position
+
+//Leave climber in the open, left and right trigger. Maybe front two bumpers.
+
+//Left, right, middle climb button.
+//Joysticks for manually moving climber arms
+//Up down on both acuators move bolth independetly on climber
+
+//Driver
+//Orient button - A, chose an area with the nav x, position a direction, EX: Push a button turn to the left, towards the other allience, look towards the opposite side
+
+//Lime light button - Y, Orients and points at an april tag.
+
+//180 on AMP april tag?
