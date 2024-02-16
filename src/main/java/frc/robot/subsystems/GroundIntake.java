72b4bdadeca2d2c01402644e8dbfbea565e2f68f
Team 9567 Programing <Team9567Prog@gmail.com>
2024-02-15 20:44:47 -0800

    Skeleton of Ground Intake code

--- src/main/java/frc/robot/subsystems/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/GroundIntake.java
@@ -2,6 +2,11 @@ package frc.robot.subsystems;
 
 //imports
 import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import java.lang.annotation.Target;
+
+import org.ejml.equation.Function;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkLimitSwitch;
@@ -14,119 +19,189 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.GroundIntakeConstants;
+import frc.robot.Constants.GroundIntakeConstants.GroundIntakeStateMachine;
 import frc.robot.Constants.RobotChassisConstants;
 import frc.robot.subsystems.LinearActuatorHomer;
 
 //Subystem class
 public class GroundIntake extends SubsystemBase {
 
-    // Variables
-    // Current groundIntakePivotAngle
-    // Curent groundIntakeHeight
-    // Current groundIntakeState
-    // Current groundIntakeSpeed, MotorOutake Speed, Dejamming/shooting
-    // 2 Home switches
-    // When note is in -- Boolean
+        // Variables
+        // Current groundIntakePivotAngle
+        // Curent groundIntakeHeight
+        // Current groundIntakeState
+        // Current groundIntakeSpeed, MotorOutake Speed, Dejamming/shooting
+        // 2 Home switches
+        // When note is in -- Boolean
 
-    // Constants
-    // Enum Height refering to state -- Constant for Home
-    // Enum Pivot angle refering to State -- Angles for StorageLow, StorageHigh,
-    // Ground, Unknown, Inside perimeter storage low -- DECLRES THE ANGLES FOR THE
-    // STATE
-    // Enum intakeSpeed
-    // Enum state machine -- DECLARES THE STATES
-    // Distance when note is in
+        // Constants
+        // Enum Height refering to state -- Constant for Home
+        // Enum Pivot angle refering to State -- Angles for StorageLow, StorageHigh,
+        // Ground, Unknown, Inside perimeter storage low -- DECLRES THE ANGLES FOR THE
+        // STATE
+        // Enum intakeSpeed
+        // Enum state machine -- DECLARES THE STATES
+        // Distance when note is in
 
-    // Motors
-    // M8 and M9, linear acuators for height.
-    // M10, Motor for pivot
-    // M11 and M12, Intaking notes.
-    // Make one of the arm motors follow the other arm
+        // Motors
+        // M8 and M9, linear acuators for height.
+        // M10, Motor for pivot
+        // M11 and M12, Intaking notes.
+        // Make one of the arm motors follow the other arm
 
-    public GroundIntakeConstants.GroundIntakeStateMachine intakeState = GroundIntakeConstants.GroundIntakeStateMachine.S0_Unknown;
+        public GroundIntakeStateMachine currentIntakeState = GroundIntakeStateMachine.S0_Unknown;
+        public GroundIntakeStateMachine targetState;
 
-    // Linear Motors -- Height
-    public CANSparkMax heightPositionLeftMotor = new CANSparkMax(GroundIntakeConstants.kHeightPositionLeftDeviceID,
-            MotorType.kBrushless);
-    public CANSparkMax heightPositionRightMotor = new CANSparkMax(GroundIntakeConstants.kHeightPositionRightDeviceID,
-            MotorType.kBrushless);
 
-    // Rotational Motors -- Pivot
-    public CANSparkMax pivotPositionMotor = new CANSparkMax(GroundIntakeConstants.kPivotPositionMotorDeviceID,
-            MotorType.kBrushless); // TODO Motor typeCheck later
+        //public GroundIntakeStateMachine.enum unknownState = GroundIntakeStateMachine.S0_Unknown;
 
-    // RPM motors -- Intake
-    public CANSparkMax leftIntakeRPMMotor = new CANSparkMax(GroundIntakeConstants.kLeftIntakeRPMMotorDeviceID,
-            MotorType.kBrushless); // TODO Motor typeCheck later
-    public CANSparkMax rightIntakeRPMMotorFollower = new CANSparkMax(GroundIntakeConstants.kRightIntakeRPMMotor_Follower,
-            MotorType.kBrushless); // TODO Motor typeCheck later
 
-    // Encoders -- For the height
-    public RelativeEncoder heightPositionLeftEncoder = heightPositionLeftMotor.getEncoder();
-    public RelativeEncoder heightPositionRightEncoder = rightIntakeRPMMotorFollower.getEncoder();
 
-    // Encoders -- For Pivot
-    public RelativeEncoder pivotAngleEncoder = pivotPositionMotor.getEncoder();
 
-    // Encoders -- For RPM motors
-    public RelativeEncoder leftGroundIntakeEncoder = leftIntakeRPMMotor.getEncoder();
-    public RelativeEncoder rightGroundIntakeEncoder = rightIntakeRPMMotorFollower.getEncoder();
 
-    // Declare NoteSensor
-    LaserCan noteSensor = new LaserCan(GroundIntakeConstants.kLaserCanId);
 
-    //Make the homing switches
-        LinearActuatorHomer leftElevatorHomer = new LinearActuatorHomer(GroundIntakeConstants.kLeftElevatorLimit, GroundIntakeConstants.kLeftElevatorHomingSpeed, heightPositionLeftMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed));
-        LinearActuatorHomer rightElevatorHomer = new LinearActuatorHomer(GroundIntakeConstants.kRightElevatorLimit, GroundIntakeConstants.kRightElevatorHomingSpeed, heightPositionRightMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed));
+        // Linear Motors -- Height
+        public CANSparkMax heightPositionLeftMotor = new CANSparkMax(GroundIntakeConstants.kHeightPositionLeftDeviceID,
+                        MotorType.kBrushless);
+        public CANSparkMax heightPositionRightMotor = new CANSparkMax(
+                        GroundIntakeConstants.kHeightPositionRightDeviceID,
+                        MotorType.kBrushless);
 
-    public GroundIntake() {
-        //Code for getting a measurement from a laser can
-        //LaserCan.Measurement measurement = noteSensor.getMeasurement();
+        // Rotational Motors -- Pivot
+        public CANSparkMax pivotPositionMotor = new CANSparkMax(GroundIntakeConstants.kPivotPositionMotorDeviceID,
+                        MotorType.kBrushless); // TODO Motor typeCheck later
 
-        for (CANSparkMax m : new CANSparkMax[] { heightPositionLeftMotor, heightPositionRightMotor, pivotPositionMotor,
-                leftIntakeRPMMotor, rightIntakeRPMMotorFollower }) {
-            m.clearFaults();
-            m.setIdleMode(RobotChassisConstants.kMotorBrakeMode);
-            m.setSmartCurrentLimit(RobotChassisConstants.kCurrentLimit, RobotChassisConstants.kCurrentLimit);
-            m.setOpenLoopRampRate(RobotChassisConstants.rampRate);
+        // RPM motors -- Intake
+        public CANSparkMax leftIntakeRPMMotor = new CANSparkMax(GroundIntakeConstants.kLeftIntakeRPMMotorDeviceID,
+                        MotorType.kBrushless); // TODO Motor typeCheck later
+        public CANSparkMax rightIntakeRPMMotorFollower = new CANSparkMax(
+                        GroundIntakeConstants.kRightIntakeRPMMotor_Follower,
+                        MotorType.kBrushless); // TODO Motor typeCheck later
+
+        // Encoders -- For the height
+        public RelativeEncoder heightPositionLeftEncoder = heightPositionLeftMotor.getEncoder();
+        public RelativeEncoder heightPositionRightEncoder = rightIntakeRPMMotorFollower.getEncoder();
+
+        // Encoders -- For Pivot
+        public RelativeEncoder pivotAngleEncoder = pivotPositionMotor.getEncoder();
+
+        // Encoders -- For RPM motors
+        public RelativeEncoder leftGroundIntakeEncoder = leftIntakeRPMMotor.getEncoder();
+        public RelativeEncoder rightGroundIntakeEncoder = rightIntakeRPMMotorFollower.getEncoder();
+
+        // Declare NoteSensor
+        LaserCan noteSensor = new LaserCan(GroundIntakeConstants.kLaserCanId);
+
+        // Make the homing switches
+        LinearActuatorHomer leftElevatorHomer = new LinearActuatorHomer(GroundIntakeConstants.kLeftElevatorLimit,
+                        GroundIntakeConstants.kLeftElevatorHomingSpeed,
+                        heightPositionLeftMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed));
+        LinearActuatorHomer rightElevatorHomer = new LinearActuatorHomer(GroundIntakeConstants.kRightElevatorLimit,
+                        GroundIntakeConstants.kRightElevatorHomingSpeed,
+                        heightPositionRightMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed));
+
+        public GroundIntake() {
+                // Code for getting a measurement from a laser can
+                // LaserCan.Measurement measurement = noteSensor.getMeasurement();
+
+                for (CANSparkMax m : new CANSparkMax[] { heightPositionLeftMotor, heightPositionRightMotor,
+                                pivotPositionMotor,
+                                leftIntakeRPMMotor, rightIntakeRPMMotorFollower }) {
+                        m.clearFaults();
+                        m.setIdleMode(RobotChassisConstants.kMotorBrakeMode);
+                        m.setSmartCurrentLimit(RobotChassisConstants.kCurrentLimit,
+                                        RobotChassisConstants.kCurrentLimit);
+                        m.setOpenLoopRampRate(RobotChassisConstants.rampRate);
+                }
+                // Declare encoders
+                // Height left and right
+                heightPositionLeftEncoder.setPosition(0);
+                heightPositionRightEncoder.setPosition(0);
+
+                // PIVOT ANGLE, Absolute incoder
+                pivotAngleEncoder.setPosition(0);
+
+                // Ground intake left and right
+                leftGroundIntakeEncoder.setPosition(0);
+                rightGroundIntakeEncoder.setPosition(0);
+
+                // Declaring the right ground intake motor a follower
+                rightIntakeRPMMotorFollower.follow(leftIntakeRPMMotor);
         }
-        // Declare encoders
-        // Height left and right
-        heightPositionLeftEncoder.setPosition(0);
-        heightPositionRightEncoder.setPosition(0);
 
-        // PIVOT ANGLE, Absolute incoder
-        pivotAngleEncoder.setPosition(0);
+        public void periodic() {
+                // rightElevatorHomer.softStopTest(heightPositionRightMotor);
+                // leftElevatorHomer.softStopTest(heightPositionLeftMotor);
+                rightElevatorHomer.homeStep(heightPositionRightMotor);
+                leftElevatorHomer.homeStep(heightPositionLeftMotor);
+                transitionState();
+        } 
 
-        // Ground intake left and right
-        leftGroundIntakeEncoder.setPosition(0);
-        rightGroundIntakeEncoder.setPosition(0);
+        pub
 
-        //Declaring the right ground intake motor a follower
-        rightIntakeRPMMotorFollower.follow(leftIntakeRPMMotor);
-    }
-    public void periodic(){
-       // rightElevatorHomer.softStopTest(heightPositionRightMotor);
-        // leftElevatorHomer.softStopTest(heightPositionLeftMotor);
-        rightElevatorHomer.homeStep(heightPositionRightMotor);
-        leftElevatorHomer.homeStep(heightPositionLeftMotor);
-    } 
+        public void transitionState() {
+                if (currentIntakeState == targetState) {
+                        return;
+                }
 
-    public void transitionState(GroundIntakeConstants.GroundIntakeStateMachine targetState){
-        if (intakeState == targetState) {
-                // nothing to do     
+                var nextState = currentIntakeState.nextStepTo(targetState);
+
+                //var previousState = getIntakeState(currentIntakeState, targetState);
+
+                //Takes the current state and advances next state forward
+                if (currentIntakeState == GroundIntakeStateMachine.S0_Unknown
+                                && nextState == GroundIntakeStateMachine.S1_GroundIntake) {
+                        S0ToS1();
+
+                } else if (currentIntakeState == GroundIntakeStateMachine.S1_GroundIntake
+                                && nextState == GroundIntakeStateMachine.S2_PositionLow) {
+                        S1ToS2();
+
+                } else if (currentIntakeState == GroundIntakeStateMachine.S2_PositionLow
+                                && nextState == GroundIntakeStateMachine.S3_PositionHigh) {
+                        S2ToS3();
+
+                } else if (currentIntakeState == GroundIntakeStateMachine.S3_PositionHigh
+                                && nextState == GroundIntakeStateMachine.S4_PositionDunk) {
+                        S3ToS4();
+
+                //Takes the current state and advances nest state backward
+                } else if (currentIntakeState == GroundIntakeStateMachine.S2_PositionLow
+                                && nextState == GroundIntakeStateMachine.S1_GroundIntake) {
+                        S2ToS1();
+
+                } else if (currentIntakeState == GroundIntakeStateMachine.S3_PositionHigh
+                                && nextState == GroundIntakeStateMachine.S2_PositionLow) {
+                        S3ToS2();
+
+                } else if (currentIntakeState == GroundIntakeStateMachine.S4_PositionDunk
+                                && nextState == GroundIntakeStateMachine.S3_PositionHigh){
+                        S4ToS3();
+                } 
         }
 
-        var nextState = getNextState(intakeState, targetState);
+        public  GroundIntakeStateMachine advanceCurrentState(GroundIntakeStateMachine targetState){
+                
+                /*
+                if(currentIntakeState == GroundIntakeStateMachine.S0_Unknown){
+                        return GroundIntakeStateMachine.S1_GroundIntake;
+
+                } else if (currentIntakeState == GroundIntakeStateMachine.S1_GroundIntake) {
+                         return GroundIntakeStateMachine.S2_PositionLow;
+
+                } else if (currentIntakeState == GroundIntakeStateMachine.S2_PositionLow) {
+                         return GroundIntakeStateMachine.S3_PositionHigh;
+
+                } else if (currentIntakeState == GroundIntakeStateMachine.S3_PositionHigh) {
+                         return GroundIntakeStateMachine.S4_PositionDunk;
+                } else if (currentIntakeState == GroundIntakeStateMachine.S4_PositionDunk) {
+                         return GroundIntakeStateMachine.S2_PositionLow;
+                }
+        }
+        */
+
         
-        if (intakeState == S0 && nextState == S1) {
-                // doS0ToS1()...
-        } else if (...) {
-                // doS1ToS2
-        }
-
-    }
-
-    // need a helper function that takes a "now" state and a "target" state, and returns the next step state
+        // need a helper function that takes a "now" state and a "target" state, and
+        // returns the next step state
 
 }
